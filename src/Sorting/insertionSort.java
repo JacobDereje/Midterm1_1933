6eab4b467b74c09c67a697b3a3793bf91fc4d3af
Jacob Dereje <Jacobdereje@gmail.com>
2022-10-17 03:31:31 -0500

    stuff

--- src/Sorting/insertionSort.java
+++ src/Sorting/insertionSort.java
@@ -1,5 +1,5 @@
 package Sorting;
-
+// The opposite of bubble sort, checks behind not ahead
 public class insertionSort {
     public static void main(String[] args) {
         int array[] = {9,1,8,2,7,3,6,4,5};
